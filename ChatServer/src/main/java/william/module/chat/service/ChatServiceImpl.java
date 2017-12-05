024b6379fd416b9ddbbd2b011765f11bdc777da9
ZhangShenao <15201440436@163.com>
2017-12-05 20:37:54 +0800

    发送私聊消息测试

--- ChatServer/src/main/java/william/module/chat/service/ChatServiceImpl.java
+++ ChatServer/src/main/java/william/module/chat/service/ChatServiceImpl.java
@@ -1,6 +1,13 @@
 package william.module.chat.service;
 
 import org.springframework.stereotype.Component;
+import william.core.session.SessionManager;
+import william.module.ModuleId;
+import william.module.chat.ChatCmd;
+import william.module.chat.proto.ChatModule;
+import william.module.chat.proto.ChatModule.ChatResponse;
+import william.module.chat.proto.ChatModule.ChatResponse.Builder;
+import william.module.chat.proto.ChatModule.ChatType;
 import william.util.LogUtil;
 
 /**
@@ -20,6 +27,21 @@ public class ChatServiceImpl implements ChatService{
 	@Override
 	public void privateChat(long senderkey, long receiverKey, String content) {
 		LogUtil.info("playerKey:" + senderkey + "向: " + receiverKey + " 发送消息: " + content);
+		
+		//创建消息对象
+		Builder builder = ChatModule.ChatResponse.newBuilder();
+		ChatResponse chatResponse = builder.setChatType(ChatType.PRIVATE)
+		.setSendPlayerId(senderkey)
+		.setMessage(content)
+		.setSendPlayerName("James")
+		.setTargetPlayerName("Kobe")
+		.build();
+		
+		//向目标玩家发送数据
+		SessionManager.sendMsgToSession(receiverKey, ModuleId.CHAT, ChatCmd.PUSHCHAT, chatResponse);
+		
+		//给自己也回一个
+		SessionManager.sendMsgToSession(senderkey, ModuleId.CHAT, ChatCmd.PUSHCHAT, chatResponse);
 	}
 
 }
