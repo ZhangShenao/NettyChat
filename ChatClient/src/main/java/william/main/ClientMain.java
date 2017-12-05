14a071fcaa80cb5b91043e0196d5b720fbf4976c
ZhangShenao <15201440436@163.com>
2017-12-05 08:54:25 +0800

    添加ignore

--- ChatClient/src/main/java/william/main/ClientMain.java
+++ ChatClient/src/main/java/william/main/ClientMain.java
@@ -2,7 +2,6 @@ package william.main;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-
 import william.swing.SwingClient;
 
 /**
