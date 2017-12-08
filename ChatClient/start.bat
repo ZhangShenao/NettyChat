df09b9ddc03b812a2c4fae772bdba60e5159a750
ZhangShenao <15201440436@163.com>
2017-12-08 15:46:52 +0800

    添加启动脚本

---
+++ ChatClient/start.bat
@@ -0,0 +1,4 @@
+@echo off  
+set CLASSPATH=.;%CLASSPATH%;.\ChatClient-0.0.1-SNAPSHOT.jar
+java -jar ChatClient-0.0.1-SNAPSHOT.jar 
+pause 
---
+++ ChatServer/start.bat
@@ -0,0 +1,4 @@
+@echo off  
+set CLASSPATH=.;%CLASSPATH%;.\ChatServer-0.0.1-SNAPSHOT.jar   
+java -jar ChatServer-0.0.1-SNAPSHOT.jar  
+pause 
